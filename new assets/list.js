cee8e281032729db3870705b705aeb20edccb277
Daniel Grujic <grujicdaniel22@gmail.com>
2023-09-16 23:34:33 +0200

    NewUpdates

--- new assets/list.js
+++ new assets/list.js
@@ -1,3 +1,8 @@
+function setHalfVolume() {
+  var myAudio = document.getElementById("audio1");
+  myAudio.volume = 0.5; //Changed this to 0.5 or 50% volume since the function is called Set Half Volume ;)
+}
+
 const pokeForm = document.getElementById("all-pokemon");
 
 pokeForm.addEventListener("submit", function (e) {
